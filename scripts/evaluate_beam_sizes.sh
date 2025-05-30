c496ad0217f44b314979bfec178e8518deb089a8
Marc Amsler <marc.amsler.02@gmail.com>
2025-05-30 12:19:49 +0200

    abc

--- scripts/evaluate_beam_sizes.sh
+++ scripts/evaluate_beam_sizes.sh
@@ -9,7 +9,7 @@ for beam in 1 2 3 5 10 15 20 25; do
     echo "Beam size $beam..."
     CONFIG_FILE="${CONFIG_PREFIX}${beam}.yaml"
     
-    echo CONFIG_FILE
+    echo "$CONFIG_FILE"
 
     if [ ! -f "$CONFIG_FILE" ]; then
         echo "❌ Config file $CONFIG_FILE not found. Skipping..."
