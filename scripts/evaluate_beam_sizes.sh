c701ca4adec5a59ac30f6401a254b4e02cce2b88
Marc Amsler <marc.amsler.02@gmail.com>
2025-05-30 16:53:45 +0200

    aa

--- scripts/evaluate_beam_sizes.sh
+++ scripts/evaluate_beam_sizes.sh
@@ -4,7 +4,7 @@ CONFIG_PREFIX=configs/configs_beam_size/bpe_5k_beam
 OUTDIR=beam_eval_results
 mkdir -p "$OUTDIR"
 
-for beam in 1 2 3 5 10 15 20 25; do
+for beam in 1 2 3 4 5 7 10 15 20 25; do
     echo "🔍 Beam size $beam..."
     CONFIG_FILE="${CONFIG_PREFIX}${beam}.yaml"
 
